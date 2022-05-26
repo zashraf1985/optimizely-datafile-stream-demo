5da610216450085447d101596d93bcc62438334f
Mike Chu <michael.chu@optimizely.com>
2022-05-26 11:18:40 -0400

    Consumer-side edits for realtime via ably

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
-import { createInstance, enums, setLogLevel } from '@optimizely/optimizely-sdk';
+import {createInstance} from '@optimizely/optimizely-sdk';
+import Ably from "ably/callbacks";
 
 const optimizely = createInstance({
-  sdkKey: 'Vro3NpoQyjkv7jQAaQeH8'
+    sdkKey: '41W6e8Z6JgB87DKE8Ych8',
+    enableRealtimeUpdateNotification: true,
+    enableStreaming: false,
 });
 
 optimizely?.onReady().then((d) => {
-  document.getElementById("flag1").innerHTML = "flag1";
-})
+    document.getElementById("flag1").innerHTML = "flag1";
+});
+
+// Admin API Key
+// const realtime = new Ably.Realtime('1ZS5wg.jAEL8A:8SDXX7o4lEI2NSDToNWetGjvb52gY5BlpMmfqnjah38');
+const realtime = new Ably.Realtime('8bjSyw.MGAWTA:upb01Uzp9GcHmP0Y01QCvpbZUZl3GQCqnbB6xHsVIEQ');
+const channel = realtime.channels.get("21468570738_development");
+
+document.addEventListener("readystatechange", readyEvent => {
+    const { readyState } = readyEvent.target as Document;
+    if (readyState !== 'complete') {
+        return;
+    }
+
+    const sendPushButton = document.getElementById("send-push-button");
+    sendPushButton.addEventListener("click", clickEvent => {
+        clickEvent.preventDefault();
+        console.log("Clicked manual push button on client.");
+        channel.publish("datafile-updated", {"datafile": "{JSON String Here}"});
+    });
+});
+
