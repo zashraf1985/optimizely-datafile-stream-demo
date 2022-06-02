73de6ee460dac6df8533c3f470d35c8f1c16aa8f
Mike Chu <michael.chu@optimizely.com>
2022-06-02 17:53:56 -0400

    WIP: Somewhat demo ready but battling CORS in develrc

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,44 @@
-import {createInstance} from '@optimizely/optimizely-sdk';
+import {createInstance, OptimizelyUserContext, OptimizelyDecision} from '@optimizely/optimizely-sdk';
 
 const optimizely = createInstance({
-    sdkKey: 'Dp4dLTSVkoP8VhYkdb4Z4',
+    sdkKey: 'FYDMrz5n9uRYLfyF15CLm',
     datafileOptions: {
-        enableRealtimeUpdateNotification: false,
-        enableStreaming: true,
+        enableRealtimeUpdateNotification: true,
         ablyApiKey: '1ZS5wg.g5H34w:Q9kxDvOBljjdhQ5LcK9eTWqp64e4b5iHioyT3zI_5_g',
-    }
+        urlTemplate: 'https://optimizely-staging.s3.amazonaws.com/datafiles/%s.json'
+    },
 });
 
-optimizely?.onReady().then((d) => {
-    document.getElementById("flag1").innerHTML = "flag1";
+const updateHeroTitle = (decision: OptimizelyDecision): void => {
+    const title  = decision.variables[decision.variationKey];
+    document.getElementsByTagName("h1").textContent = title;
+}
+
+const updateHeroImage = (decision: OptimizelyDecision): void => {
+    const imageUrl  = decision.variables[decision.variationKey];
+    document.getElementsByClassName("hero-container").style.backgroundImage = `url('${imageUrl}')`;
+}
+
+optimizely?.onReady().then(() => {
+    const HERO_TITLE_EXPERIMENT = 'hero_title';
+    const HERO_IMAGE_EXPERIMENT = 'hero_image';
+
+    const userContext: OptimizelyUserContext = optimizely.createUserContext('user-673e7c9d-2b2a-49e3-9faf-9d7216ecd1fe');
+
+    const heroTitleDecision = userContext.decide(HERO_TITLE_EXPERIMENT);
+    updateHeroTitle(heroTitleDecision);
+
+    const heroImageDecision = userContext.decide(HERO_IMAGE_EXPERIMENT);
+    updateHeroImage(heroImageDecision);
+
+    userContext.subscribeToFlag(HERO_TITLE_EXPERIMENT, (decision: OptimizelyDecision)=>{
+        console.log('Hero 🎉 title updated.');
+        console.dir(decision);
+        updateHeroTitle(decision);
+    });
+    userContext.subscribeToFlag(HERO_IMAGE_EXPERIMENT, (decision: OptimizelyDecision)=>{
+        console.log('Hero 🖼️ image updated.');
+        console.dir(decision);
+        updateHeroImage(decision);
+    });
 });
